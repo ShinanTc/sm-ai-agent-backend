02c021febab14579baadb3b28748c2649abf497a
Shinan Tc <shinantc1@gmail.com>
2025-05-27 20:04:04 +0530

    several path corrects made in carosel generator

--- generators/generate_carousel.py
+++ generators/generate_carousel.py
@@ -16,11 +16,11 @@ else:
     text_color = "#dcfb73"  # Default text color for green background
 
 # Paths
-text_file = "content.txt"
-heading_template = f"templates/carousel-contents/bg-{bg_choice}/heading.png"
-content_template = f"templates/carousel-contents/bg-{bg_choice}/content.png"
-output_folder = "generated_carousel"
-font_path = "assets/BricolageGrotesque_72pt-Bold.ttf"
+text_file = "../content.txt"
+heading_template = f"../templates/carousel-contents/bg-{bg_choice}/heading.png"
+content_template = f"../templates/carousel-contents/bg-{bg_choice}/content.png"
+output_folder = "../generated_carousel"
+font_path = "../assets/BricolageGrotesque_72pt-Bold.ttf"
 
 # Font settings
 font_size = 60
